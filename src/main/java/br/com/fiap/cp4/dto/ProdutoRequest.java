3ff760cd77f5d700782ab3d816d337e7b0327e2a
Otavio Miklos <otaviomiklos@gmail.com>
2025-08-18 15:49:19 -0300

    chore: service main

--- src/main/java/br/com/fiap/cp4/dto/ProdutoRequest.java
+++ src/main/java/br/com/fiap/cp4/dto/ProdutoRequest.java
@@ -3,7 +3,14 @@ package br.com.fiap.cp4.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProdutoRequest {
 
     @NotBlank
--- src/main/java/br/com/fiap/cp4/service/ProdutoService.java
+++ src/main/java/br/com/fiap/cp4/service/ProdutoService.java
@@ -1,8 +1,8 @@
 package br.com.fiap.cp4.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,30 +19,65 @@ public class ProdutoService {
     private ProdutoRepository produtoRepository;
 
     public ProdutoResponse create(ProdutoRequest produtoRequest){
-        return new ProdutoResponse();
+        Produto produto = produtoRepository.save(ProdutoService.toProduto(produtoRequest));
+        return ProdutoService.toResponse(produto);
     }
 
     public List<ProdutoResponse> readAll(){
-        return new ArrayList<ProdutoResponse>();
+        List<Produto> produtos = produtoRepository.findAll();
+        return ProdutoService.toResponse(produtos);
     }
 
     public Optional<ProdutoResponse> readById(Long id){
-        return Optional.ofNullable(null);
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if(produto.isEmpty()) return Optional.empty();
+        return Optional.of(ProdutoService.toResponse(produto.get()));
     }
 
     public Optional<ProdutoResponse> update(Long id, ProdutoRequest produtoRequest){
-        return Optional.ofNullable(null);
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if(produto.isEmpty()) return Optional.empty();
+
+        Produto newProduto = toProduto(produtoRequest);
+        newProduto.setId(id);
+        
+        produtoRepository.save(newProduto);
+        return Optional.of(ProdutoService.toResponse(newProduto));
     }
 
     public boolean delete(Long id){
-        return false;
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if(produto.isEmpty()) return false;
+
+        produtoRepository.delete(produto.get());
+        return true;
     }
 
-    private ProdutoResponse toResponse(Produto produto){
-        return new ProdutoResponse();
-    } 
+    private static ProdutoResponse toResponse(Produto produto){
+        ProdutoResponse produtoResponse = new ProdutoResponse();
+        produtoResponse.setId(produto.getId());
+        produtoResponse.setNome(produto.getNome());
+        produtoResponse.setTipo(produto.getTipo());
+        produtoResponse.setSetor(produto.getSetor());
+        produtoResponse.setTamanho(produto.getTamanho());
+        produtoResponse.setPreco(produto.getPreco());
+        // produtoResponse.setLinks();
 
-    private Produto toProduto(ProdutoRequest produtoRequest){
-        return new Produto();
+        return produtoResponse;
+    }
+
+    private static List<ProdutoResponse> toResponse(List<Produto> produtos){
+        return produtos.stream().map(ProdutoService::toResponse).collect(Collectors.toList());
+    }
+
+    private static Produto toProduto(ProdutoRequest produtoRequest){
+        Produto produto = new Produto();
+        produto.setNome(produtoRequest.getNome());
+        produto.setTipo(produtoRequest.getTipo());
+        produto.setSetor(produtoRequest.getSetor());
+        produto.setTamanho(produtoRequest.getTamanho());
+        produto.setPreco(produtoRequest.getPreco());
+
+        return produto;
     }
 }
